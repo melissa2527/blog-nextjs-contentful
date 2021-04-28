b1203346ac6679147ad13e2d49326e0b4632df5a
annie2527 <you@example.com>
2021-04-28 23:13:22 +0200

    fix slug page

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -2,46 +2,60 @@ import {createClient} from 'contentful';
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
 
-let client = createClient({
+const client = createClient({
     space: process.env.NEXT_CONTENTFUL_SPACE_ID,
     accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN
   })
 
-export async function getStaticPaths() {
+export const getStaticPaths = async () => {
     const res = await client.getEntries({
-        content_type: 'blog'
+        content_type: 'blog',
+    })
+
+    const paths = res.items.map(item => {
+        return {
+            params: {slug: item.fields.slug}
+        }
     })
 
     return {
-        paths: res.items.map(article => ({
-            params: {slug: article.fields.slug}
-        })),
+        paths,
         fallback: true
     }
+
+    // return {
+    //     paths: res.items.map(article => ({
+    //         params: {slug: article.fields.slug}
+    //     })),
+    //     fallback: true
+    // }
 }
 
 export async function getStaticProps({params}) {
-    const res = await client.getEntries({
+    const {items} = await client.getEntries({
         content_type: 'blog',
         'fields.slug': params.slug
     })
 
     return {
         props: {
-            article: res.items[0]
-        }
+            blog: items[0]
+        },
+        revalidate: 1
     }
 }
 
-export default function Article({article}) {
-    const {image, title} = article.fields
+export default function Article({blog}) {
+    if (!blog) return <div>404</div>
+
+    const {image, title} = blog.fields
     return (
         <div>
-            <h1>{article.fields.title}</h1>
+            <h1>{title}</h1>
             <Image src={`https:${image.fields.file.url}`} alt={title} 
             width={image.fields.file.details.image.width}
             height={image.fields.file.details.image.height}/>
-            <div>{documentToReactComponents(article.fields.text)}</div>
+            <div>{documentToReactComponents(blog.fields.text)}</div>
         </div>
     )
 }
